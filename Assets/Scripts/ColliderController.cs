4cbefecd7fe3927ac2d32e089963a9df02220920
Alejandro Camús <alcaval1999@gmail.com>
2022-04-09 13:52:22 +0200

    ayaya

--- Assets/Scripts/ColliderController.cs
+++ Assets/Scripts/ColliderController.cs
@@ -6,6 +6,9 @@ public class ColliderController : MonoBehaviour
 {
     private GameObject car;
     private CarController carController;
+    [SerializeField] TrailController trailController;
+    [SerializeField] private bool _switch = false;
+    [SerializeField] private ColliderController cc;
 
     private void Awake() {
         car = GameObject.FindGameObjectWithTag("Car");
@@ -13,6 +16,26 @@ public class ColliderController : MonoBehaviour
     }
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("ei");
+        //ContactPoint2D[] contacts;
+        //int ncontacts = other.GetContacts(contacts)[0];
+        if(carController.isCarDrifting(out float latVelocity, out bool isDrifting) && other.tag == "Car" && _switch)
+        {
+            Debug.Log("Tremendo loop");
+            trailController.ClearPoints(true);
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                Debug.Log("Point of contact: " + hit.point);
+                trailController.setPolygon(hit.point);
+            }
+            cc.clearSelf();
+        }
+    }
+
+    
+
+    public void clearSelf()
+    {
+        trailController.ClearPoints(true);
     }
 }
--- Assets/Scripts/TrailController.cs
+++ Assets/Scripts/TrailController.cs
@@ -9,6 +9,7 @@ public class TrailController : MonoBehaviour
     Vector3[] positions = new Vector3[1000];
 
     [SerializeField] EdgeCollider2D ec;
+    [SerializeField] private PolygonCollider2D _detectionArea;
 
     private void Awake() {
         carController = GetComponentInParent<CarController>();
@@ -39,16 +40,42 @@ public class TrailController : MonoBehaviour
         }
         else
         {
-            StartCoroutine(ClearPointsRoutine());
-            positions = new Vector3[1000];
-            tr.emitting = false;
+            ClearPoints(false);
         }
     }
 
-    IEnumerator ClearPointsRoutine()
+    public void ClearPoints(bool boom)
     {
+        StartCoroutine(ClearPointsRoutine(boom));
+        positions = new Vector3[1000];
+        tr.emitting = false;
+    }
+
+    IEnumerator ClearPointsRoutine(bool boom)
+    {
+        if(boom)
+        {
+            tr.Clear();  
+            //_detectionArea.points = ec.points;
+        } 
         yield return new WaitForSeconds(0.2f);
         ec.Reset();
         ec.isTrigger = true;
+    }
+
+    public void setPolygon(Vector2 hit)
+    {
+        List<Vector2> newPoints = new List<Vector2>();
+        
+        foreach(Vector2 v in ec.points)
+        {
+            if(Vector2.Distance(hit,v) > 0.2)
+            {
+                print("y tal");
+                newPoints.Add(v);
+            }
+        }
+
+        _detectionArea.points = newPoints.ToArray();
     }
 }
