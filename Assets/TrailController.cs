5eb33427d6fb0d05366f22fcf4922524d516f79b
Alejandro Camús <alcaval1999@gmail.com>
2022-04-09 11:30:37 +0200

    car cosas

--- Assets/TrailController.cs
+++ Assets/TrailController.cs
@@ -6,6 +6,9 @@ public class TrailController : MonoBehaviour
 {
     private CarController carController;
     private TrailRenderer tr;
+    Vector3[] positions = new Vector3[1000];
+
+    [SerializeField] EdgeCollider2D ec;
 
     private void Awake() {
         carController = GetComponentInParent<CarController>();
@@ -26,9 +29,33 @@ public class TrailController : MonoBehaviour
         if(carController.isCarDrifting(out float latVelocity, out bool isDrifting))
         {
             tr.emitting = true;
+            
+            //Vector3[] positions = [Vector3.zero];
+            int i = tr.GetPositions(positions);
+
+            
+            List<Vector2> lista = new List<Vector2>();
+            for(int j = 0; j < positions.Length - 1; j++)
+            {
+                if(positions[j].x != 0 || positions[j].y != 0) lista.Add(new Vector2(positions[j].x, positions[j].y));
+                //v[j] = new Vector2(positions[j].x, positions[j].y);
+            }
+            Vector2[] v = lista.ToArray();
+
+            ec.points = v;
+            //print(positions);
+            // Mesh mesh = new Mesh();
+            // tr.BakeMesh(mesh);
+            // mesh.Optimize();
+            // mesh.RecalculateNormals();
+            // mesh.RecalculateBounds();
+            // mesh.RecalculateTangents();
+            // Instantiate(mesh);
         }
         else
         {
+            ec.points = new Vector2[0];
+            positions = new Vector3[1000];
             tr.emitting = false;
         }
     }
