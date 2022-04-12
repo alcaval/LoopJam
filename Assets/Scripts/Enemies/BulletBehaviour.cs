37b5233af2c66a9296bb3fb0c5101577829b08ba
dededec <daviderenacasado@gmail.com>
2022-04-12 03:03:13 +0200

    ahora las balas hacen daño y desaparecen
    trabaja plasma las palabras hazlas balas

--- Assets/Scripts/Enemies/BulletBehaviour.cs
+++ Assets/Scripts/Enemies/BulletBehaviour.cs
@@ -18,17 +18,41 @@ namespace LoopJam
 
         [Tooltip("Bullet speed when created.")]
 	    [SerializeField] private float _speed;
+	    [SerializeField] private float _lifeTime;
+
 	  
 	    #endregion
 	  
 	    #region LifeCycle
 	  
         // Start, OnAwake, Update, etc
-        private void Start() 
+        private void OnEnable()   
         {
             GetComponent<Rigidbody2D>().velocity = transform.right * _speed;
+            StartCoroutine(crDestroy());
+        }
+
+        private void OnDisable() 
+        {
+            StopAllCoroutines();
         }
 
         #endregion
+
+        private IEnumerator crDestroy()
+        {
+            yield return new WaitForSeconds(_lifeTime);
+            gameObject.SetActive(false);
+        }
+
+        private void OnCollisionEnter2D(Collision2D other) 
+        {
+            if(other.gameObject.tag == "Car")
+            {
+                other.gameObject.GetComponent<CarController>().takeDamage(0.3f);
+            }    
+            
+            gameObject.SetActive(false);
+        }
     }
 }
